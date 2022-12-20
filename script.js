462f51b7bfbf521c12924b06dfc03b5cfbd513fb
neliken <nelicacechina@gmail.com>
2022-12-20 11:31:38 +0200

    02.refactoring

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 
-function split(el) {
+function splitArrayElements(el) {
     const values = el.split(" ");
-    let endpoints = values[0];
+    const endpoint = values[0];
     let ids = values[1];
 
     if (ids == undefined) {
@@ -11,10 +11,10 @@ function split(el) {
         ids = JSON.parse(values[1]);
     }
 
-    return { endpoints, ids };
+    return { endpoint, ids };
 }
 
-function filterArray(item, index, array) {
+function regroupDublicates(item, index, array) {
     if (array.map(() => item['endpoints']).indexOf(item['endpoints']) === index)  {
         item.ids = item.ids.concat(array[index + 1].ids);
         array.splice(index, 1)
@@ -22,59 +22,61 @@ function filterArray(item, index, array) {
 }
 
 async function asyncReadFile(filename) {
-        try {
+    try {
         const contents = await fs.promises.readFile(filename, 'utf-8');
-        const arr = contents.trim().split(/\r?\n/);
+        const arrayData = contents.trim().split(/\r?\n/);
 
-        const obj = arr.map((el) => split(el));
+        let objectsArray = arrayData.map((el) => splitArrayElements(el));
         
-        const filtredArray = obj.filter((item, index, array) => {
-            filterArray(item, index, array);
-            return obj;
+        objectsArray = objectsArray.filter((item, index, array) => {
+            regroupDublicates(item, index, array);
+            return objectsArray;
         });
 
-        return filtredArray;
+        return objectsArray;
 
     } catch (err) {
         console.log(err);
     }
 }
 
-function writeFile(txtFileName, data) {
-    fs.writeFile("textFiles/" + txtFileName + ".txt", data, (err) => {
+function writeNewFile(fileName, data) {
+    fs.writeFile("textFiles/" + fileName + ".txt", data, (err) => {
         if (err) throw err;
     });
 } 
 
-async function getData() {
-    const obj = await asyncReadFile('./text.txt');
-    let URL = "https://jsonplaceholder.typicode.com/";
+async function fetchData(currentURL) {
+    const response = await fetch(currentURL);
+    const data = await response.text();
+    return data;
+}
 
-    for (let keys in obj ) {
-        let currentEndpoint = obj[keys].endpoints;
-        let currentIds = obj[keys].ids;
+async function dataHandler() {
+    const objectsArray = await asyncReadFile('./text.txt');
+    const URL = "https://jsonplaceholder.typicode.com/";
+
+    for (let keys in objectsArray) {
+        let currentEndpoint = objectsArray[keys].endpoint;
+        let currentIds = objectsArray[keys].ids;
 
         if(!currentIds.length) {
-            let currentURL = URL + currentEndpoint;
+            const currentURL = URL + currentEndpoint;
+            const fileName = currentEndpoint;
 
-            const response = await fetch(currentURL);
-            const data = await response.text();
-
-            let txtFileName = currentEndpoint;
-            writeFile(txtFileName, data);
+            const data = await fetchData(currentURL);
+            writeNewFile(fileName, data);
         }
 
         for(let id in currentIds) {
-            let currentID = currentIds[id];
-            let currentURL  = URL + currentEndpoint + "/" + currentID;
+            const currentID = currentIds[id];
+            const currentURL  = URL + currentEndpoint + "/" + currentID;
+            const fileName = currentEndpoint + currentID;
     
-            const response = await fetch(currentURL);
-            const data = await response.text();
-            
-            let txtFileName = currentEndpoint + currentID;
-            writeFile(txtFileName, data);
+            const data = await fetchData(currentURL);
+            writeNewFile(fileName, data);
         }
     }      
 }
 
-getData()
+dataHandler();
